ff9891a152ab4ebe7980833b1f28d4e2aa8e19d7
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-02-21 16:55:35 +0300

    create custom user admin

--- AutoDocxOnline/documents/admin.py
+++ AutoDocxOnline/documents/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Document, DocumentsPackage, User
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
+
+class MyUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    form = MyUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+            (None, {'fields': ()}),
+    )
+
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
@@ -12,6 +29,3 @@ class DocumentAdmin(admin.ModelAdmin):
     search_fields = ('name', 'file')
     list_filter = ('name',)
     empty_value_display = '-пусто-'
-
-
-admin.site.register(User)
