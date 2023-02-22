81b43f2b269bc4539e48a97c364e6915c8d7d6f8
Alexey-zaliznuak <zaliznuak20@gmail.com>
2023-02-22 20:21:50 +0300

    update documentAdmin model

--- AutoDocxOnline/documents/admin.py
+++ AutoDocxOnline/documents/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.urls import reverse
 from .models import Document, DocumentsPackage, User
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
+from django.http import request
 
 
 class MyUserChangeForm(UserChangeForm):
@@ -21,11 +23,21 @@ class MyUserAdmin(UserAdmin):
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = (
-        'pk',
         'name',
-        'file',
+        'public',
         'owner',
+        'file_path',
+        'pk',
     )
-    search_fields = ('name', 'file')
+    search_fields = ('name', 'owner__username',)
     list_filter = ('name',)
     empty_value_display = '-пусто-'
+
+    @admin.display(empty_value='unknown', description="load_path")
+    def file_path(self, obj):
+        return reverse(
+            'upload',
+                args=(
+                obj.pk,
+            )
+        )
